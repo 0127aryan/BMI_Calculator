b1ab08a8c9ee298d5d1d66021c3a8a4c56825b5e
Aryan Khandelwal <aryankhandelwal0127@gmail.com>
2024-09-01 17:08:20 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useMemo, useState } from 'react'
+import './App.css'
 
-function App() {
+const App = () => {
+  const [height, setHeight] = useState(170);
+  const [weight, setWeight] = useState(80);
+
+  function onWeightChange(event) {
+      setWeight(event.target.value)
+  }
+
+  function onHeightChange(event){
+      setHeight(event.target.value)
+  }
+  const output = useMemo(() => {
+    const calculateHeight = height /100;
+    return(weight/ (calculateHeight*calculateHeight)).toFixed(1)
+
+  },[weight,height])
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <h1>
+        BMI Calculator
+      </h1>
+      <div className='inputSection'>
+      <p className='sliderOutput'>Weight:{weight}</p>
+      <input 
+      className='inputSlider' 
+      type='range' 
+      step='1' 
+      min='40' 
+      max='200'
+      onChange={onWeightChange} />
+
+      <p className='sliderOutput'> Height: {height}cm</p>
+      <input 
+      className='inputSlider'
+      type='range'
+      min='100'
+      max='270'
+      onChange={onHeightChange} />
+
+      <div className='outputSection'>
+      <p>Your BMI is</p>
+      <p className='output'>{output}</p>
+         
+      </div>
+
+
+      
+
+      </div>
+
+      <div>
+        <img src='https://imgs.search.brave.com/HK094T4M05SXToYTCtfcXdK2ax_iB6FjqTNz1kofflk/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzA1LzY0Lzc5LzUx/LzM2MF9GXzU2NDc5/NTE5MV9WU0FPbGRR/Vmg0eTBuMXp1eEtO/ODVvS25HSlpQak1o/bi5qcGc' alt='BMI Scale' className='bmiImage'></img>
+      </div>
+    </main>
+  )
 }
 
-export default App;
+export default App
